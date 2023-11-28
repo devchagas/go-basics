060adf2bc07d81f04d704ad36e61b1e1c1760740
devgabrielchagas <gabriel.chagas2044@gmail.com>
2023-11-28 10:07:11 -0300

    add: programa sobre strings

--- tipos/string/string.go
+++ tipos/string/string.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	fmt.Println("O tamanho da string é = ", len("Hello World"))
 
-	fmt.Println("Hello World"[2])
+	fmt.Println("Hello World"[2]) //Le o codigo binario, l = 108
 
 	fmt.Println("Hello" + "World")
 }
