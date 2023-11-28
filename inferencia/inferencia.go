08e21fb58677559e7f762cf4ef33221168345b25
devgabrielchagas <gabriel.chagas2044@gmail.com>
2023-11-28 10:21:20 -0300

    add: programa sobre inferencia de tipos

---
+++ inferencia/inferencia.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome = "Gabriel" //var nome string = "Gabriel"
+	var idade = 23       //var idade int = 23
+	var versao = 2.2     //var versao float32 = 2.2
+
+	fmt.Println("Olá Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+}
